563cda1072307b76cfc1c8f24b162ac45c01c1c3
sansika <it21028014@my.sliit.lk>
2023-11-22 17:26:44 +0530

    first basic commit

---
+++ email-auth/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [first, setFirst] = useState("Zino");
+
+  return (
+    <>
+      <p1>Hello World</p1>
+    </>
+  );
+}
+
+export default App;
